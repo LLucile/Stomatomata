07869d58027174f9c3f14e10be316f78a07ecb92
Sicarde <camille.koziar@gmail.com>
2017-03-18 21:06:01 +0000

    brochettes (placeholders)

--- Assets/Scripts/ParseLevels.cs
+++ Assets/Scripts/ParseLevels.cs
@@ -9,18 +9,22 @@ public class ParseLevels : MonoBehaviour {
         public List<string> skewerList;
         public string description;
     }
+    [HideInInspector]
     public List<LevelStruct> levels;
+    public List<GameObject> levelsGameObjects;
+    public GameObject skewerPrefab;
     void Start() {
         levels = new List<LevelStruct>();
+        levelsGameObjects = new List<GameObject>();
         ReadLevels();
-        for (int i = 0; i < levels.Count; ++i) {
+        /*for (int i = 0; i < levels.Count; ++i) {
             LevelStruct lvl = getLevel(i);
             Debug.Log(lvl.levelNumber);
             foreach (string skewer in lvl.skewerList) {
                 Debug.Log(skewer);
             }
             Debug.Log(lvl.description);
-        }
+        }*/
     }
     public LevelStruct getLevel(int levelNb) {
         if (levels.Count > levelNb) {
@@ -28,6 +32,53 @@ public class ParseLevels : MonoBehaviour {
         }
         Debug.Log("Level was not loaded");
         return levels[0];
+    }
+    public void createLevelGameObject(LevelStruct level) {
+        GameObject obj = new GameObject();
+        obj.name = level.levelNumber.ToString();
+        Vector3 pos = new Vector3(2.0f, 2.0f, .0f);
+        float step = .8f;
+        for (int i = 0; i < level.skewerList.Count; ++i) {
+            GameObject metaStick = new GameObject();
+            metaStick.transform.SetParent(obj.transform);
+            metaStick.name = "metaStick";
+            GameObject stick = Instantiate(skewerPrefab);
+            stick.name = "stick";
+            stick.transform.SetParent(metaStick.transform);
+            stick.transform.localPosition = pos + new Vector3(step * ((level.skewerList[i].Length - 4.0f) / 2.0f), .0f, .0f);
+            stick.transform.localScale = new Vector3(level.skewerList[i].Length + 2.0f, .5f, 1.0f);
+            stick.GetComponent<SpriteRenderer>().color = Color.black;
+            stick.GetComponent<SpriteRenderer>().sortingOrder = 4;
+            GameObject eatable = Instantiate(skewerPrefab);
+            eatable.name = "isEatable";
+            eatable.transform.SetParent(metaStick.transform);
+            eatable.transform.localPosition = pos - new Vector3(step, .0f, .0f);
+            if (level.skewerList[i][0] == 'x') {
+                eatable.GetComponent<SpriteRenderer>().color = Color.magenta;
+            } else {
+                eatable.GetComponent<SpriteRenderer>().color = Color.white;
+            }
+            for (int j = 1; j < level.skewerList[i].Length; ++j) {
+                GameObject skewElem = Instantiate(skewerPrefab);
+                skewElem.name = "nomnomnom";
+                if (level.skewerList[i][j] == 'r') {
+                    skewElem.GetComponent<SpriteRenderer>().color = Color.red;
+                } else if (level.skewerList[i][j] == 'g') {
+                    skewElem.GetComponent<SpriteRenderer>().color = Color.green;
+                } else {
+                    skewElem.GetComponent<SpriteRenderer>().color = Color.blue;
+                }
+                skewElem.transform.SetParent(metaStick.transform);
+                skewElem.transform.localPosition = pos;
+                pos.x += step;
+            }
+            pos.y += step;
+            pos.x = 2.0f;
+        }
+        levelsGameObjects.Insert(level.levelNumber - 1, obj);
+        if (level.levelNumber != 1) {
+            obj.SetActive(false);
+        }
     }
     public void ReadLevels() {
         StreamReader inputStream = new StreamReader("Assets/Scenes/levels.txt");
@@ -45,7 +96,8 @@ public class ParseLevels : MonoBehaviour {
                 inputLine = inputStream.ReadLine();
             }
             levelRead.description = inputLine;
-            levels.Add(levelRead);
+            levels.Insert(levelRead.levelNumber - 1, levelRead);
+            createLevelGameObject(levelRead);
         }
         inputStream.Close();
     }

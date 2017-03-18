7a88d0622aecae22c115a1de009709a029b7b8c6
Sicarde <camille.koziar@gmail.com>
2017-03-18 21:56:28 +0000

    potential check for transitions

--- Assets/Scripts/Transition.cs
+++ Assets/Scripts/Transition.cs
@@ -3,17 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Transition : MonoBehaviour {
-
+    public enum tType {
+        Red,
+        Green,
+        Blue,
+        NotRed,
+        NotGreen,
+        NotBlue,
+        All
+    };
     public GameObject endNode;
     public GameObject startNode;
+    public tType transitionType;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool eat(string skewer) {
+        if (skewer[skewer.Length - 1] == 'r') {
+            if (transitionType == tType.Green || transitionType == tType.Blue || transitionType == tType.NotRed) {
+                return false;
+            }
+        } else if (skewer[skewer.Length - 1] == 'g') {
+            if (transitionType == tType.Red || transitionType == tType.Blue || transitionType == tType.NotGreen) {
+                return false;
+            }
+        } else {
+            if (transitionType == tType.Red || transitionType == tType.Green || transitionType == tType.NotBlue) {
+                return false;
+            }
+        }
+        Node nextNode = endNode.GetComponent<Node>();
+        skewer = skewer.Substring(0, skewer.Length - 2);
+        for (int i = 0; i < nextNode.transitions.Count; ++i) {
+            if (nextNode.transitions[i].GetComponent<Transition>().eat(skewer)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

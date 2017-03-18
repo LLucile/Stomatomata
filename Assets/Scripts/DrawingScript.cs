c2c082150a73d708cfe815be4c675918225a1a47
Lucile <lucile@ymail.com>
2017-03-18 19:50:57 +0100

    Drawing graphs is now possible
    Though those graphs do not do anything and you cant add new nodes !

--- Assets/Scripts/DrawingScript.cs
+++ Assets/Scripts/DrawingScript.cs
@@ -20,6 +20,7 @@ public class DrawingScript : MonoBehaviour {
     private float angle = 0f;
     private Vector3 currentMousePosDelta;
     private bool startedNewTrans = false;
+    private float mouseDistance = 0f;
 
     // Use this for initialization
     void Start()
@@ -35,15 +36,17 @@ public class DrawingScript : MonoBehaviour {
             if(mouseDown == true){
                 Debug.Log("hey mouse is down !");
                 beginNode = this.objectOver;
-                Debug.Log("It clicked " + beginNode.name);
-                if (beginNode.CompareTag("Node"))
-                {
-                    beginNode.GetComponent<Node>().CreateTrans(beginNode.transform.position);
-                    startedNewTrans = true;
-                }
-                else
-                {
-                    Debug.Log("sorry but this object does not qualify as a beginning node");
+                if(beginNode != null){
+                    Debug.Log("It clicked " + beginNode.name);
+                    if (beginNode.CompareTag("Node"))
+                    {
+                        beginNode.GetComponent<Node>().CreateTrans(beginNode.transform.position);
+                        startedNewTrans = true;
+                    }
+                    else
+                    {
+                        Debug.Log("sorry but this object does not qualify as a beginning node");
+                    }
                 }
             }
             else
@@ -57,52 +60,22 @@ public class DrawingScript : MonoBehaviour {
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - Camera.main.transform.position.z));
             currentMousePosDelta = mousePosition - beginNode.transform.position;
+            //lets compute angle
             currentTrans.transform.localEulerAngles = new Vector3(0, 0, Mathf.Atan2((mousePosition.y - beginNode.transform.position.y), (mousePosition.x - beginNode.transform.position.x)) * Mathf.Rad2Deg);
             Debug.Log("hey check that nice angle transition has ! Angle = " + currentTrans.transform.localEulerAngles.z);  
+            //lets compute distance
+            mouseDistance = Mathf.Sqrt(currentMousePosDelta.x*currentMousePosDelta.x + currentMousePosDelta.y*currentMousePosDelta.y);
+            Debug.Log("hey check that nice distance mouse has ! Distance = " + mouseDistance);
+            if (mouseDistance > 1)
+            {
+                currentTrans.transform.localScale = new Vector3(mouseDistance, mouseDistance, currentTrans.transform.localScale.z);
+            }
+            else
+            {
+
+            }
         }
         previousmouseDown = mouseDown;
 	}
-
-    /*public bool startedNewTrans = false;
-    public GameObject currentTrans;
-    public Vector3 endNodeCoords;
-    public Vector3 beginNodeCoords;
-
-    private float angle = 0f;
-    private Vector3 currentMousePosDelta;
-    
-
-
-    //script variables
-    bool mouseDown;
-    private bool previousmouseDown = false;
-    private bool mouseReleased = true;
-
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        mouseDown = Input.GetMouseButton(0);
-
-        if (previousmouseDown != mouseDown && mouseDown == true)
-        {
-            beginNodeCoords = 
-        }
-        if (Input.GetMouseButton(0) && startedNewTrans)
-        {
-            currentMousePosDelta = Input.mousePosition - beginNodeCoords;
-            angle = Mathf.Cos(currentMousePosDelta.y / currentMousePosDelta.x);
-            currentTrans.transform.eulerAngles = new Vector3(0, 0, angle);
-        }
-     //   Debug.Log("hey check that nice angle " + angle);
-        previousmouseDown = mouseDown;
-	}
-     * */
-
-    
 
 }

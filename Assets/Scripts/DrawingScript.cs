fce06ae3cdfcf81643ebada5878b8f0d5a7de423
Lucile <lucile@ymail.com>
2017-03-19 11:40:33 +0100

    L'editeur fonctionne presque bien ! :D
    TATATATAAAAAAAAAAAA

--- Assets/Scripts/DrawingScript.cs
+++ Assets/Scripts/DrawingScript.cs
@@ -9,6 +9,12 @@ public class DrawingScript : MonoBehaviour
     public GameObject nodePrefab;
     public GameObject bouclePrefab;
 
+    public Sprite stomachSprite;
+    public Sprite selfLoopSprite;
+    public Sprite normalSprite;
+
+    public float mouseDistanceMin = 0.4f;
+
     public float drawingCanvasMinX = -5f;
     public float drawingCanvasMaxX = 5f;
     public float drawingCanvasMinY = -5f;
@@ -143,8 +149,23 @@ public class DrawingScript : MonoBehaviour
                                 Transition t = currentTrans.GetComponent<Transition>();
                                 t.endNode = this.endNode;
                                 float dist = Vector3.Distance(t.startNode.transform.position, t.endNode.transform.position);
-                                currentTrans.transform.localScale = new Vector3(dist / 2, dist / 2, currentTrans.transform.localScale.z); //snap (middle of node)
-                                if (c.gameObject == t.startNode) {
+                                //change sprite for stomach sprite if endNode == "EstomacFinal"
+                                if (endNode.name == "EstomacFinal")
+                                {
+                                    Debug.Log("Snapping on Stomach !!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                                    currentTrans.transform.localScale = new Vector3(dist / 2, (currentTrans.transform.localScale.y / Mathf.Abs(currentTrans.transform.localScale.y)) * dist / 2, currentTrans.transform.localScale.z); //snap (middle of node)
+                                    this.currentTrans.GetComponentInChildren<SpriteRenderer>().sprite = stomachSprite;
+                                }
+                                //Debug.Log("endNode : " + this.endNode.name + " start Node : " + t.startNode);
+                                if (this.endNode != t.startNode)
+                                {
+                                    currentTrans.transform.localScale = new Vector3(dist / 2, (currentTrans.transform.localScale.y / Mathf.Abs(currentTrans.transform.localScale.y)) * dist / 2, currentTrans.transform.localScale.z); //snap (middle of node)
+                                }
+                                else
+                                {
+                                    currentTrans.transform.localScale = new Vector3(1, (currentTrans.transform.localScale.y / Mathf.Abs(currentTrans.transform.localScale.y)) * 1, 1); //snap (middle of node)
+                                }
+                                    /*if (c.gameObject == t.startNode) {
                                     GameObject boucleTrans = Instantiate(bouclePrefab);
                                     boucleTrans.GetComponent<Transition>().startNode = this.endNode;
                                     boucleTrans.GetComponent<Transition>().endNode = this.endNode;
@@ -152,9 +173,11 @@ public class DrawingScript : MonoBehaviour
                                     Destroy(this.currentTrans);
                                     this.currentTrans = boucleTrans;
                                     boucleTrans.GetComponent<Transition>().startNode.GetComponent<Node>().transitions.Add(this.currentTrans);
-                                    boucleTrans.transform.position = endNode.transform.position + new Vector3(.0f, 1.0f, .0f);
-                                }
+                                    boucleTrans.transform.position = endNode.transform.position;// +new Vector3(.0f, 1.0f, .0f);
+                                }*/
                                 done = true;
+                                this.currentTrans = null;
+                                this.startedNewTrans = false;
                             }
                         }
                     }
@@ -181,20 +204,29 @@ public class DrawingScript : MonoBehaviour
         if (mouseDown && startedNewTrans)
         {
             currentMousePosDelta = mousePosition - beginNode.transform.position;
+            Debug.Log("mouse distance = " + mouseDistance);
             //lets compute angle
             currentTrans.transform.localEulerAngles = new Vector3(0, 0, Mathf.Atan2((mousePosition.y - beginNode.transform.position.y), (mousePosition.x - beginNode.transform.position.x)) * Mathf.Rad2Deg);
+            angle = currentTrans.transform.localEulerAngles.z;
             //Debug.Log("hey check that nice angle transition has ! Angle = " + currentTrans.transform.localEulerAngles.z);
             //lets compute distance
             mouseDistance = Mathf.Sqrt(currentMousePosDelta.x * currentMousePosDelta.x + currentMousePosDelta.y * currentMousePosDelta.y);
             //Debug.Log("hey check that nice distance mouse has ! Distance = " + mouseDistance);
-            if (mouseDistance > 1)
+            if (angle > 90 && angle < 270 || angle < -90 && angle > -270)
+            {
+                Debug.Log("Inversing sprite");
+                currentTrans.transform.localScale = new Vector3(currentTrans.transform.localScale.x, -1 * Mathf.Abs(currentTrans.transform.localScale.y), currentTrans.transform.localScale.z);
+            }
+            if (mouseDistance > mouseDistanceMin)
             {
                 // TO DO : here we will need to add the sprite change for scale changes
-                currentTrans.transform.localScale = new Vector3(mouseDistance / 2, mouseDistance / 2, currentTrans.transform.localScale.z);
+                this.currentTrans.GetComponentInChildren<SpriteRenderer>().sprite = normalSprite;
+                currentTrans.transform.localScale = new Vector3(mouseDistance / 2, (currentTrans.transform.localScale.y / Mathf.Abs(currentTrans.transform.localScale.y) )* mouseDistance / 2, currentTrans.transform.localScale.z);
             }
             else
             {
                 //TO DO : here use self-node transition sprite
+                this.currentTrans.GetComponentInChildren<SpriteRenderer>().sprite = selfLoopSprite;
             }
         }
         previousmouseDown = mouseDown;
